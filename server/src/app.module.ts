bcba66fab0905ab5ca7c7dfdf3df3c8c8e18f252
Adekunbi Adefolarin <dekunbifola100@gmail.com>
2024-03-14 14:23:15 +0100

    chore: remove unnecessary files

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,12 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { UrlModule } from './url/url.module';
 
 @Module({
   imports: [ConfigModule.forRoot(), UrlModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
