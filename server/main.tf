591f4b309ff2644e354e04c1abb30ed88a69e61f
Adekunbi Adefolarin <dekunbifola100@gmail.com>
2024-03-24 22:39:40 +0100

    refactor: redis service

--- server/main.tf
+++ server/main.tf
@@ -15,14 +15,44 @@ resource "aws_elasticache_cluster" "shortener_redis" {
   security_group_ids = [aws_security_group.shortener_redis_sg.id]
 }
 
+resource "aws_instance" "url-shortener" {
+  ami                   = "ami-0183b16fc359a89dd" # Make sure this AMI is available in your region and supports your instance type
+  instance_type         = "t3.micro"
+  key_name              = "folababa" # Replace with your actual SSH key name
+
+  # Ensure the instance is associated with the same security group as the Redis cluster
+  vpc_security_group_ids = [aws_security_group.shortener_redis_sg.id]
+
+  tags = {
+    Name = "ExampleEC2Instance"
+  }
+}
+
 resource "aws_security_group" "shortener_redis_sg" {
   name        = "shortener-redis-sg"
-  description = "Allow inbound traffic for Redis"
+  description = "Allow inbound traffic for Redis and SSH"
 
+  # Redis port
   ingress {
     from_port   = 6379
     to_port     = 6379
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"] # Warning: This allows access from anywhere. Adjust accordingly.
+  }
+
+  # SSH port
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Adjust this to a more restricted range for better security
+  }
+
+  # Default rule to allow outbound traffic
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
--- server/src/common/db/redis/redis.service.ts
+++ server/src/common/db/redis/redis.service.ts
@@ -1,12 +1,20 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import Redis from 'ioredis';
 
 @Injectable()
 export class RedisService {
   private readonly redis: Redis;
+  private readonly logger = new Logger(RedisService.name);
 
   constructor() {
-    this.redis = new Redis(process.env.REDIS_URL);
+    const options = {
+      host: process.env.REDIS_HOST,
+      port: Number(process.env.REDIS_PORT),
+    };
+    this.redis = new Redis(options);
+
+    this.redis.on('connect', () => this.logger.log('Connected to Redis.'));
+    this.redis.on('error', (error) => this.logger.error('Redis error', error));
   }
 
   async get(key: string): Promise<string | null> {
@@ -14,7 +22,12 @@ export class RedisService {
   }
 
   async set(key: string, value: string): Promise<string> {
-    return this.redis.set(key, value, 'EX', process.env.CACHE_EXPIRY_TIME);
+    return this.redis.set(
+      key,
+      value,
+      'EX',
+      Number(process.env.REDIS_EXPIRY_TIME),
+    );
   }
 
   async del(key: string): Promise<number> {
